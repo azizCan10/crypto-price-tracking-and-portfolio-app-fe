59fc49ec671a7308a235373420719ebd2a383503
Aziz Can Öz <oz.azizcann@gmail.com>
2024-08-09 20:56:56 +0300

    login page created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
 
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+import axios from "axios";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from './pages/Login';
+import  React from 'react';
+
 function App() {
+    axios.defaults.baseURL = 'http://localhost:8080/api';
+
     return (
         <div className="App">
-            <header className="App-header">
-            </header>
+            <Router>
+                <Routes>
+                    <Route index element={<Login/>} />
+                </Routes>
+            </Router>
         </div>
     );
 }
---
+++ src/pages/Login.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+
+import { Link } from 'react-router-dom';
+import 'react-toastify/dist/ReactToastify.css';
+import axios from "axios";
+
+export default function Login() {
+
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+    const login = async (e) => {
+        e.preventDefault();
+
+        const requestBody = {
+            username: username,
+            password: password,
+        };
+
+        try {
+            await axios.post(`/auth/generate-token`, requestBody, {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+
+            alert('Giriş başarılı');
+        } catch (error) {
+            if (error.response.status === 404) {
+                alert("Böyle bir kullanıcı bulunmamaktadır!");
+            } else {
+                alert("Kullanıcı adı ya da şifre hatalı!");
+            }
+        }
+    };
+
+    return (
+        <div className="d-flex justify-content-center align-items-center vh-100">
+            <div className="card p-4" style={{ width: '350px', backgroundColor: "#f8f9fa" }}>
+                <h3 className="text-center mb-4">Giriş Yap</h3>
+                <form>
+                    <div className="mb-3">
+                        <label htmlFor="userName" className="form-label">Kullanıcı Adı</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            name="userName"
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="password" className="form-label">Şifre</label>
+                        <input
+                            type="password"
+                            className="form-control"
+                            name="password"
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </div>
+                    <div className="d-grid">
+                        <Link className="btn btn-success mx-2" onClick={login}>Giriş Yap</Link>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+}
