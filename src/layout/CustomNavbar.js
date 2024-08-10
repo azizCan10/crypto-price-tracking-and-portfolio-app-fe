ab589f27e3075ef410850232fed96aabeea42e6b
Aziz Can Öz <oz.azizcann@gmail.com>
2024-08-10 16:59:23 +0300

    navbar fix

--- src/layout/CustomNavbar.js
+++ src/layout/CustomNavbar.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import {Navbar} from "react-bootstrap";
 
-export default function Navbar() {
+export default function CustomNavbar() {
     return (
         <Navbar collapseOnSelect expand="lg" className="custom-navbar">
             <Container>
