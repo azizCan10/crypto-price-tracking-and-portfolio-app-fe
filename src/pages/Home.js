a8c2d288d98d88ae4fdd9170ecd9767b1b1e9438
Aziz Can Öz <oz.azizcann@gmail.com>
2024-08-11 14:27:16 +0300

    user crypto track add and delete services added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { TOKEN } from "../config";
 import TradingViewWidget from "../widget/TradingViewWidget";
 import HomeNavbar from "../layout/HomeNavbar";
+import { Modal, Button } from 'react-bootstrap';
 
 export default function Home() {
     const [me, setMe] = useState(null);
@@ -10,6 +11,8 @@ export default function Home() {
     const [prices, setPrices] = useState({});
     const [previousPrices, setPreviousPrices] = useState({});
     const [admin, setAdmin] = useState(false);
+    const [showModal, setShowModal] = useState(false);
+    const [modalSymbol, setModalSymbol] = useState('');
 
     const getMe = async () => {
         try {
@@ -88,35 +91,97 @@ export default function Home() {
         return isNaN(numericPrice) ? 'Loading...' : numericPrice.toFixed(4);
     };
 
+    const handlePlusClick = () => {
+        setShowModal(true);
+    };
+
+    const handleModalSubmit = async () => {
+        try {
+            await axios.post('/user-crypto-track', {
+                crypto: {
+                    symbol: modalSymbol,
+                }
+            }, {
+                headers: {
+                    'Authorization': `Bearer ${TOKEN}`,
+                    'Content-Type': 'application/json'
+                },
+            });
+            setShowModal(false);
+            getMe();
+        } catch (error) {
+            console.error('API call error:', error);
+        }
+    };
+
+    const handleXClick = async (cryptoId) => {
+        try {
+            await axios.delete(`/user-crypto-track/${cryptoId}`, {
+                headers: {
+                    'Authorization': `Bearer ${TOKEN}`,
+                    'Content-Type': 'application/json'
+                },
+            });
+            getMe();
+        } catch (error) {
+            console.error('API call error:', error);
+        }
+    };
+
     return (
         <>
             <HomeNavbar isAdmin={admin} />
             <div className="d-flex" style={{ height: 'calc(100vh - 56px)' }}>
-                <div style={{ width: '80%' }}>
+                <div style={{ width: '75%' }}>
                     <TradingViewWidget symbol={selectedSymbol} />
                 </div>
 
-                <div style={{ width: '20%' }}>
+                <div style={{ width: '25%' }}>
                     <table className="custom-table">
                         <thead>
                         <tr>
                             <th scope="col">Tracking</th>
                             <th scope="col"></th>
+                            <th scope="col" onClick={handlePlusClick} style={{ cursor: 'pointer' }}>+</th>
                         </tr>
                         </thead>
                         <tbody>
                         {me && me.tracking && me.tracking.map((x, index) => (
                             <tr key={index} onClick={() => handleSymbolClick(x.symbol)}>
                                 <td>{x.symbol}</td>
-                                <td style={{ color: getPriceColor(x.symbol) }}>
+                                <td style={{color: getPriceColor(x.symbol)}}>
                                     {formatPrice(prices[x.symbol])}
                                 </td>
+                                <td scope="col" onClick={() => handleXClick(x.id)} style={{ cursor: 'pointer' }}>X</td>
                             </tr>
                         ))}
                         </tbody>
                     </table>
                 </div>
             </div>
+
+            <Modal show={showModal} onHide={() => setShowModal(false)} className="custom-modal">
+                <Modal.Header closeButton>
+                    <Modal.Title>Track New Crypto</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Enter symbol"
+                        value={modalSymbol}
+                        onChange={(e) => setModalSymbol(e.target.value)}
+                    />
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={() => setShowModal(false)}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={handleModalSubmit}>
+                        Track Symbol
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </>
     );
 }
